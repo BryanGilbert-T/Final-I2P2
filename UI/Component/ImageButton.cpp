59186eda2c88443cd75350a366c35f912fdb1236
Bryan Gilbert <bryangilbert1411@gmail.com>
2025-06-14 20:41:17 +0800

    Added sfx button

--- UI/Component/ImageButton.cpp
+++ UI/Component/ImageButton.cpp
@@ -1,8 +1,10 @@
 #include <functional>
 #include <memory>
+#include <iostream>
 
 #include "Engine/Collider.hpp"
 #include "Engine/GameEngine.hpp"
+#include "Engine/AudioHelper.hpp"
 #include "Engine/Point.hpp"
 #include "Engine/Resources.hpp"
 #include "Image.hpp"
@@ -20,13 +22,23 @@ namespace Engine {
     }
     void ImageButton::OnMouseDown(int button, int mx, int my) {
         if ((button & 1) && mouseIn && Enabled) {
+            AudioHelper::PlaySample("sfx/buttonclick.mp3", false,
+                    AudioHelper::SFXVolume, 0);
             if (OnClickCallback)
                 OnClickCallback();
         }
     }
     void ImageButton::OnMouseMove(int mx, int my) {
         mouseIn = Collider::IsPointInBitmap(Point((mx - Position.x) * GetBitmapWidth() / Size.x + Anchor.x * GetBitmapWidth(), (my - Position.y) * GetBitmapHeight() / Size.y + Anchor.y * GetBitmapHeight()), bmp);
-        if (!mouseIn || !Enabled) bmp = imgOut;
-        else bmp = imgIn;
+        if (!mouseIn || !Enabled) {
+            bmp = imgOut;
+        }
+        else {
+            if (bmp == imgOut) {
+                AudioHelper::PlaySample("sfx/buttonhover.mp3", false,
+                    AudioHelper::SFXVolume, 0);
+            }
+            bmp = imgIn;
+        }
     }
 }
